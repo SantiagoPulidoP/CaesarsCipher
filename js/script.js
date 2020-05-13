5a3964c3f52d3718323d8743878025f1d3b3b1e0
Santiago Pulido <santiagopulidopelaez@gmail.com>
2020-05-13 06:24:07 -0500

    Finished

--- js/script.js
+++ js/script.js
@@ -1,57 +1,74 @@
-var palabra = "SERR PBQR PNZC";
-var notAllowed = ['!', '@', '#', '&' ,'(', ')', '-', '[', '{', '}',']', ':',';',',', '?' ,'/', '*', "\\", '_','.','~','$','^','+','=','<','>',' '];
+/* var palabra = "SERR PBQR PNZC"; */
 
 
-var codificado = "";
-palabra = palabra.toUpperCase();
+var encode = function(){
+    var palabra = document.getElementById("value").value;
+    var codificado = "";
+    palabra = palabra.toUpperCase();
 
-//Store each letter from the word entered in an array 
-var palabra_arr = [];
-for(let i = 0; i<palabra.length; i++){
-    /* if(!notAllowed.includes(palabra[i])) */
-        palabra_arr.push(palabra[i]);
-}
-
-
-
-
-
-var cifrados = [ 
-    {main:'A',replace:'N'},
-    {main:'B',replace:'O'},
-    {main:'C',replace:'P'},
-    {main:'D',replace:'Q'},
-    {main:'E',replace:'R'},
-    {main:'F',replace:'S'},
-    {main:'G',replace:'T'},
-    {main:'H',replace:'U'},
-    {main:'I',replace:'V'},
-    {main:'J',replace:'W'},
-    {main:'K',replace:'X'},
-    {main:'L',replace:'Y'},
-    {main:'M',replace:'Z'}
- ]; 
-
-
- for(let i= 0; i<palabra_arr.length; i++){
-    for(let j=0; j<cifrados.length; j++){
-        let val = cifrados[j]["main"];
-        if(palabra_arr[i]===val){
-            palabra_arr[i]=cifrados[j].replace;
-
-        }else if (palabra_arr[i]===cifrados[j].replace){
-            palabra_arr[i]=cifrados[j].main;
-        }
-           
- }
-
-}
-
-console.log(palabra_arr);
-
-for(let k =0 ; k<palabra_arr.length ; k++){
+    document.getElementById("value").value = "";
     
-    codificado+=palabra_arr[k];   
+    //Store each letter from the word entered in an array 
+    var palabra_arr = [];
+    for(let i = 0; i<palabra.length; i++){
+        /* if(!notAllowed.includes(palabra[i])) */
+            palabra_arr.push(palabra[i]);
+    }
+    
+    
+    
+    
+    
+    var cifrados = [ 
+        {main:'A',replace:'N'},
+        {main:'B',replace:'O'},
+        {main:'C',replace:'P'},
+        {main:'D',replace:'Q'},
+        {main:'E',replace:'R'},
+        {main:'F',replace:'S'},
+        {main:'G',replace:'T'},
+        {main:'H',replace:'U'},
+        {main:'I',replace:'V'},
+        {main:'J',replace:'W'},
+        {main:'K',replace:'X'},
+        {main:'L',replace:'Y'},
+        {main:'M',replace:'Z'}
+     ]; 
+    
+    
+     for(let i= 0; i<palabra_arr.length; i++){
+        for(let j=0; j<cifrados.length; j++){
+            let val = cifrados[j]["main"];
+            if(palabra_arr[i]===val){
+                palabra_arr[i]=cifrados[j].replace;
+    
+            }else if (palabra_arr[i]===cifrados[j].replace){
+                palabra_arr[i]=cifrados[j].main;
+            }
+               
+     }
+    
+    }
+    
+    console.log(palabra_arr);
+    
+    for(let k =0 ; k<palabra_arr.length ; k++){
+        
+        codificado+=palabra_arr[k];   
+    }
+    
+    /* console.log(codificado); */
+    document.querySelector(".outcome").innerHTML = `Encoded: ${codificado}`;
+    document.querySelector(".outcome").style.display = "block";
+
+
+
+
+
 }
 
-console.log(codificado);
+
+
+
+
+document.getElementById("submit").addEventListener("click",encode);
